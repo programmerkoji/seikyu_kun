a67d814e21b513ec3686c7f8b73fa0d584be7add
Koji Sato <utautaouka@gmail.com>
2024-04-06 23:04:25 +0900

    PostingとProductのModel作成

--- app/Models/Posting.php
+++ app/Models/Posting.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Posting extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'company_id',
+        'product_id',
+        'posting_term',
+        'posting_start',
+        'quantity',
+        'is_special_price',
+        'special_price',
+        'total_price',
+        'note',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'price',
+    ];
+
+    public function postings()
+    {
+        return $this->hasMany(Posting::class);
+    }
 }
