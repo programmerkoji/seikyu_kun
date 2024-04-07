13a33f92c0bb243f9d982a75830c3e26b769be33
Koji Sato <utautaouka@gmail.com>
2024-04-07 14:12:50 +0900

    product create view add_validation_error

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -13,12 +13,18 @@
                             <div class="w-full">
                                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">商品名</label>
                                 <input type="text" id="name" name="name" value="{{old('name')}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                @error('name')
+                                <span class="text-rose-700 mt-2">{{ $message }}</span>
+                                @enderror
                             </div>
                             <div class="w-full">
                                 <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">金額</label>
                                 <div class="w-full flex items-center gap-1">
                                     <input type="text" id="price" name="price" value="{{old('price')}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">円
                                 </div>
+                                @error('price')
+                                <span class="text-rose-700 mt-2">{{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="flex items-center gap-3">
