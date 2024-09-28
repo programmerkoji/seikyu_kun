8a8f8962bd1dc05f1ea2fc7b7f40d40937ef1fa3
Koji Sato <utautaouka@gmail.com>
2024-09-29 00:00:33 +0900

    vite.config.jsの設定の再変更

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import dotenv from 'dotenv';
 
-export default defineConfig(({ mode }) => {
-    return {
-        base: process.env.VITE_BASE_PATH || '/',
-        plugins: [
-            laravel({
-                input: [
-                    'resources/css/app.css',
-                    'resources/js/app.js',
-                    'resources/js/flatpickr.js',
-                    'resources/js/autocomplete.js',
-                    'resources/css/autocomplete.css',
-                ],
-                refresh: true,
-            }),
-        ],
-    }
+dotenv.config();
+
+export default defineConfig({
+    base: process.env.VITE_BASE_PATH || '/',
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/flatpickr.js',
+                'resources/js/autocomplete.js',
+                'resources/css/autocomplete.css',
+            ],
+            refresh: true,
+        }),
+    ],
 });
